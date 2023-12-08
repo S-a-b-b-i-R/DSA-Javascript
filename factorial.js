87726301864dc5ea2363829d27caeb77b3d3d866
S-a-b-b-i-R <sabbirssmc90@gmail.com>
2023-12-09 00:27:22 +0600

    updated files

--- factorial.js
+++ factorial.js
@@ -1,9 +1,9 @@
 function factorial(n) {
-  if (n === 0) {
-    return 1;
-  } else if (n >= 1) {
-    return n * factorial(n - 1);
-  }
+    if (n === 0) {
+        return 1;
+    } else if (n >= 1) {
+        return n * factorial(n - 1);
+    }
 }
 
 console.log(fibonnaci(18));
@@ -11,34 +11,34 @@ console.log(dpFib(18));
 console.log(dpMFib(18));
 
 function fibonnaci(n) {
-  if (n >= 3) {
-    return fibonnaci(n - 1) + fibonnaci(n - 2);
-  } else {
-    return 1;
-  }
+    if (n >= 3) {
+        return fibonnaci(n - 1) + fibonnaci(n - 2);
+    } else {
+        return 1;
+    }
 }
 
 //dynamic programming using memoization
 // starting from 0 index
 function dpMFib(n, memo = {}) {
-  if (n === 1 || n === 2) {
-    return 1;
-  }
-  if (memo[n]) {
+    if (n === 1 || n === 2) {
+        return 1;
+    }
+    if (memo[n]) {
+        return memo[n];
+    }
+    memo[n] = dpMFib(n - 1, memo) + dpMFib(n - 2, memo);
+    //   console.log(memo);
     return memo[n];
-  }
-  memo[n] = dpMFib(n - 1, memo) + dpMFib(n - 2, memo);
-  //   console.log(memo);
-  return memo[n];
 }
 
 //dynamic programming function bottom up approach
 function dpFib(n) {
-  let bottomUp = [];
-  bottomUp[1] = 1;
-  bottomUp[2] = 1;
-  for (let i = 3; i <= n; i++) {
-    bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
-  }
-  return bottomUp[n];
+    let bottomUp = [];
+    bottomUp[1] = 1;
+    bottomUp[2] = 1;
+    for (let i = 3; i <= n; i++) {
+        bottomUp[i] = bottomUp[i - 1] + bottomUp[i - 2];
+    }
+    return bottomUp[n];
 }
