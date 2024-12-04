76f165b54083a4ca0b76e53dc5c66f4eee9ce0b5
S_a_b_b_i_R <sabbirssmc90@gmail.com>
2024-12-04 15:51:10 +0600

    solved howSum problem

--- howSum.js
+++ howSum.js
@@ -1,3 +1,32 @@
-const howSum = (target, arr) => {};
+const howSum = (target, arr) => {
+    if (target == 0) return [];
+    if (target < 0) return null;
 
-console.log(howSum(7, [2, 5, 4, 3]));
+    for (let num of arr) {
+        const remainder = target - num;
+        const remainderResult = howSum(remainder, arr);
+        if (remainderResult != null) {
+            return [...remainderResult, num];
+        }
+    }
+    return null;
+};
+
+const howSumMemoized = (target, arr, memo = {}) => {
+    if (target in memo) return memo[target];
+    if (target == 0) return [];
+    if (target < 0) return null;
+
+    for (let num of arr) {
+        const remainder = target - num;
+        const remainderResult = howSumMemoized(remainder, arr, memo);
+        if (remainderResult != null) {
+            memo[target] = [...remainderResult, num];
+            return memo[target];
+        }
+    }
+    memo[target] = null;
+    return null;
+};
+
+console.log(howSumMemoized(300, [7, 14]));
